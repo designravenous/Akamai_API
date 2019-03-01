20dcc9a6fc4163a77904c38df8a4a5b53aa2de94
designravenous <32999911+designravenous@users.noreply.github.com>
2019-03-01 14:53:26 +0100

    Update domain_hits.py
    No need for input of arguments anymore

--- domain_hits.py
+++ domain_hits.py
@@ -1,22 +1,8 @@
-import sys
 import datetime
 import config
 import app
 
-def check_for_argument():
-    print('--- Error ---')
-    print('--- Script should contain 3 arguments ---')
-    print('--- File type txt each domain devided by line---')
-    print('--- Startdate, example 20190101 ---')
-    print('--- Enddate, example 20190131 ---')
-    exit()
-
-try:
-    file_argument = sys.argv[1]
-    starting = sys.argv[2]
-    ending = sys.argv[3]
-except:
-    check_for_argument()
+file_argument = 'test.txt'
 
 with open(file_argument, 'r') as input_file:
     domains = input_file.readlines()
@@ -28,15 +14,8 @@ current_date = datetime.datetime.now()
 to_akamai = config.Akamai_credentials(current_date)
 s = to_akamai.Akamai_report()
 
-csv_filename = 'output_' + str(current_date.year)+str(current_date.month) + str(current_date.day) + '.csv'
-start_date = str(starting)
-end_date = str(ending)
-time_list = [csv_filename, start_date, end_date]
 
 #Calling on API Connector:
-api_call = app.Api_connector(s[0], time_list, domains, s[1])
+api_call = app.Api_connector(s[0], domains, s[1])
 #Calling method creating Report:
 api_call.Akamai_init_report()
-
-
-
